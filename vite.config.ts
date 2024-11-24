68a34bd4786d995e91bc3c70c57c928f5a0fae07
Tan Chin Siang <chinsiang@tmrnd.com.my>
2024-11-25 07:24:29 +0800

    chore: update testing dependencies and configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest/config" />
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/vitest.setup.ts'],
+    globals: true,
+    css: true,
+    testTimeout: 10000,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'text-summary', 'lcov'],
+      include: ['src/**/*.{ts,tsx}'], // Files to include for coverage
+      exclude: [
+        '**/*.config.{js,ts}',
+        'node_modules',
+        '**/*.{css,scss}',
+        'src/**/*.test.tsx', // Test files themselves are often excluded
+        'src/vitest.setup.ts',
+        'src/vite-env.d.ts',
+        'src/main.tsx'
+      ],
+    }
+  },
+});
